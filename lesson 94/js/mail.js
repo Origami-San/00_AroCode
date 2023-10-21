8e5a992382987f59cc5efd4c315a62cbd0ef9867
Origami <spb.origami@gmail.com>
2023-10-21 17:38:36 +0300

    Update Lesson 94

--- lesson 94/js/mail.js
+++ lesson 94/js/mail.js
@@ -1,3 +1,5 @@
+// Burger Menu
+
 const openBtn = document.querySelector(".burger-icon");
 const body = document.querySelector(".body");
 const nav = document.querySelector(".nav__list");
@@ -13,5 +15,36 @@ nav.addEventListener("click", (event) => {
     document.documentElement.clientWidth <= 900
   ) {
     body.classList.remove("body--opened-menu");
+  }
+});
+
+// Modal Window
+
+const openModalBtn = document.querySelector(".team__gift-btn");
+const modalWindow = document.querySelector(".modal");
+
+openModalBtn.addEventListener("click", (event) => {
+  modalWindow.classList.add("modal--open");
+  body.classList.add("body--fixed");
+  event.preventDefault();
+});
+
+modalWindow.addEventListener("click", (event) => {
+  if (
+    (event.target && event.target.classList.contains("modal")) ||
+    (event.target && event.target.classList.contains("modal__close-btn"))
+  ) {
+    modalWindow.classList.remove("modal--open");
+    body.classList.remove("body--fixed");
+  }
+});
+
+document.addEventListener("keydown", (event) => {
+  if (
+    event.code === "Escape" &&
+    modalWindow.classList.contains("modal--open")
+  ) {
+    modalWindow.classList.remove("modal--open");
+    body.classList.remove("body--fixed");
   }
 });
