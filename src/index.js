c8c940a93a796e6bcf22dab922dad79c3bde7fdf
Origami <spb.origami@gmail.com>
2024-01-03 02:15:10 +0300

    UPDATE Template

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-!function(){let s=document.getElementById("tasks"),e=document.getElementById("diplom"),t=document.getElementById("tasksBlock"),l=document.getElementById("diplomBlock"),i=document.getElementById("burger"),o=document.getElementById("sidebar"),c=document.getElementById("overlay"),d=document.getElementById("body");s.onclick=function(){e.classList.remove("active"),this.classList.add("active"),t.classList.remove("hidden"),l.classList.add("hidden"),o.classList.remove("show"),c.classList.remove("show")},e.onclick=function(){s.classList.remove("active"),this.classList.add("active"),l.classList.remove("hidden"),t.classList.add("hidden"),o.classList.remove("show"),c.classList.remove("show")},i.onclick=function(){d.classList.toggle("overflow"),c.classList.toggle("show"),o.classList.toggle("show")}}();
+(() => {
+  // Sidebar
+  const sidebarBtn = document.querySelectorAll(".sidebar__button");
+  // Content
+  const contentPage = document.querySelectorAll(".content__block");
+  const tasksBlock = document.getElementById("tasksBlock");
+  const diplomBlock = document.getElementById("diplomBlock");
+  const updateBlock = document.getElementById("updateBlock");
+  // Отслеживаем клик на каждой кнопке с классом .sidebar__button
+  sidebarBtn.forEach((btnClick) => {
+    // Навешиваем слушатель события click на btnClick
+    btnClick.addEventListener("click", function (e) {
+      // Убираем модификатор active со всех sidebarBtn
+      sidebarBtn.forEach((tabClick) => {
+        tabClick.classList.remove("active");
+      });
+      // Добавляем модификатор active к активному sidebarBtn
+      this.classList.add("active");
+      // Навешиваем класс hidden на contentPage в зависимости от нажатого sidebarBtn
+      contentPage.forEach((page) => {
+        if (this.id === "tasks") {
+          page.classList.add("hidden") & tasksBlock.classList.remove("hidden");
+        } else if (this.id === "diplom") {
+          page.classList.add("hidden") & diplomBlock.classList.remove("hidden");
+        } else if (this.id === "update") {
+          page.classList.add("hidden") & updateBlock.classList.remove("hidden");
+        }
+      });
+    });
+  });
+})();
